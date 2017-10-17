70898bd1c8206856012b800ce50b1f8522afe8b5
ahmedfekry5 <ahmed.fekry@ivas.com.eg>
2017-10-17 12:15:25 +0200

    fekry adding the restaurant model

---
+++ app/Restaurant.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Restaurant extends Model
+{
+	
+	
+    protected $table = 'restaurants';
+    protected $fillable = ['title','address'];
+
+    public function menu()
+    {
+    	return $this->hasMany('App\Menu_item','restaurant_id');
+    }
+	
+}
